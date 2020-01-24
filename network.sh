90b0dd334c678207ebee0360a1c3d61a333bf201
sarathkaleswaram <ksb994@gmail.com>
2020-01-24 13:37:25 +0530

    commented join channel from peer

--- network.sh
+++ network.sh
@@ -14,11 +14,11 @@ function networkUp() {
     exit 1
   fi
 
-  echo "Fetching the channel"
-  docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer1.org1.example.com peer channel fetch config -o orderer.example.com:7050 -c mychannel
+  # echo "Fetching the channel"
+  # docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer1.org1.example.com peer channel fetch config -o orderer.example.com:7050 -c mychannel
 
-  echo "Joining peer1.org1.example.com to the channel"
-  docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer1.org1.example.com peer channel join -b mychannel_config.block
+  # echo "Joining peer1.org1.example.com to the channel"
+  # docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer1.org1.example.com peer channel join -b mychannel_config.block
 }
 
 
